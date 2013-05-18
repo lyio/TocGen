2780b14d62421a69e6d9d0ca2175da84b410708c
Your Name <thfied@gmail.com>
2013-05-18 20:51:39 +0200

    TocGen.py

---
+++ TocGen.py
@@ -0,0 +1,17 @@
+'''
+Created on 18.05.2013
+
+@author: thomas
+'''
+
+def main(argv):
+    #checking command line parameters
+    if(len(argv) != 3):
+        print("Wrong number of arguments")
+
+
+if __name__ == '__main__':
+    # DEBUG
+    main(("fjdk"))
+    #import sys
+    #main(sys.argv[1:])
