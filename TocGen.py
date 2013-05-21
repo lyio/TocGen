5815565192d9d3321d395c476159febfdc4757c6
lyio <thfied+git@gmail.com>
2013-05-21 10:14:09 +0200

    Update TocGen.py

--- TocGen.py
+++ TocGen.py
@@ -1,17 +1,34 @@
-'''
-Created on 18.05.2013
+# parsing index.html to generate eclipse toc.xml
+import TocFile
+import lxml.html
 
-@author: thomas
-'''
+# command line parameters:
+# filepath to index.html
+# filepath to target xml file
+# filepath to the cover html file
 
-def main(argv):
-    #checking command line parameters
-    if(len(argv) != 3):
-        print("Wrong number of arguments")
+def main(args):
+    # having a look at the command line parameters
+    if len(args) != 3:
+        print("1. filepath to index.htm \n2. filepath to target xml \n3. filepath to cover html file")
+        return
+    
+    print("opening source html")
+    f = open(args[0])
+    html = f.read()
+    tree = lxml.html.fromstring(html)
+    # iterating over html
+    for e in tree.iter():
+        # getting document title    
+        if (e.tag == "p") and ("class" in e.attrib):
+            if e.attrib["class"] == "navtitle":
+                print ("Document title: " + e.text)
+                title = e.text
+                # creating XML file
+                tocXml = TocFile.TocFile(args[1], title, args[2])
 
-
-if __name__ == '__main__':
-    # DEBUG
-    main(("fjdk"))
-    #import sys
-    #main(sys.argv[1:])
+    tocXml.closeFile()
+    f.close()
+if __name__ == "__main__":
+    import sys
+    main(sys.argv[1:])
